7cf02b7c54b03787401dc9ab06ef5e8faa3538f0
kamalll31 <ilhamavenged22@gmail.com>
2025-12-31 09:24:41 +0700

    perbaiki baseurl

--- admin_web/lib/src/core/services/app_config.dart
+++ admin_web/lib/src/core/services/app_config.dart
@@ -1,9 +1,6 @@
-import 'package:flutter/foundation.dart';
-
 class AppConfig {
-  static String get apiBaseUrl => kIsWeb 
-      ? 'http://127.0.0.1:5000' 
-      : 'http://10.0.2.2:5000';
+  // Ganti alamat localhost menjadi alamat PythonAnywhere Anda
+  static String get apiBaseUrl => 'https://kamalll31.pythonanywhere.com/api/v1';
 
   static const Duration connectionTimeout = Duration(seconds: 30);
   static const Duration apiTimeout = Duration(seconds: 30);
