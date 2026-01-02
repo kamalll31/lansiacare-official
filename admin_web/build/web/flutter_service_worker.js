a43efcc9ec029a406e43355ff0c1f9112a96ca88
kamalll31 <ilhamavenged22@gmail.com>
2026-01-03 01:50:46 +0700

    FIX: api_service3

--- admin_web/build/web/flutter_service_worker.js
+++ admin_web/build/web/flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "57ea33f271b78f715102bb309a871106
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "41c2576afed84780047a0920b6a6b946",
+"flutter_bootstrap.js": "0b2c1b0b57f78c3bba69283c8d843901",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
